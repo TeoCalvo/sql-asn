da54ec2ee46502289156048dbfea27026f644a14
teocalvo2@gmail.com <teocalvo2@gmail.com>
2022-08-31 22:41:35 +0000

    Criação de todas as vairáveis de seller e produto

--- feature_store/seller_product.sql
+++ feature_store/seller_product.sql
@@ -16,36 +16,68 @@ with tb_join_all as (
   where t1.dtPurchase < '{date}'
   and t1.dtPurchase >= add_months('{date}',-6)
 
+),
+
+tb_summary as (
+
+  select idSeller,
+         avg(vlWeightGramas) as vlAvgWeight,
+         coalesce(avg(case when datediff('{date}', dtPurchase) < 30 then vlWeightGramas end),0) as vlAvgWeight1M,
+         coalesce(avg(case when datediff('{date}', dtPurchase) < 90 then vlWeightGramas end),0) as vlAvgWeight3M,
+
+         avg(nrNameLength) as vlAvgNameLength,
+         coalesce(avg(case when datediff('{date}', dtPurchase) < 30 then nrNameLength end),0) as vlAvgNameLength1M,
+         coalesce(avg(case when datediff('{date}', dtPurchase) < 90 then nrNameLength end),0) as vlAvgNameLength3M,
+
+         avg(nrPhotos) as vlAvgPhotos,
+         coalesce(avg(case when datediff('{date}', dtPurchase) < 30 then nrPhotos end),0) as vlAvgPhotos1M,
+         coalesce(avg(case when datediff('{date}', dtPurchase) < 90 then nrPhotos end),0) as vlAvgPhotos3M,
+
+         avg(vlLengthCm * vlHeightCm * vlWidthCm) as vlAvgProductVolume,
+         avg(case when datediff('{date}', dtPurchase) < 30 then vlLengthCm * vlHeightCm * vlWidthCm else 0 end) as vlAvgProductVolume1M,
+         avg(case when datediff('{date}', dtPurchase) < 90 then vlLengthCm * vlHeightCm * vlWidthCm else 0 end) as vlAvgProductVolume3M,
+
+         count(distinct idProduct) as qtProducts,
+         count(distinct case when datediff('{date}', dtPurchase) < 30 then idProduct end) as qtProducts1M,
+         count(distinct case when datediff('{date}', dtPurchase) < 90 then idProduct end) as qtProducts3M,
+
+         count(distinct descCategoryName) as qtCategoryType,
+         count(distinct case when datediff('{date}', dtPurchase) < 30 then descCategoryName end) as qtCategoryType1M,
+         count(distinct case when datediff('{date}', dtPurchase) < 90 then descCategoryName end) as qtCategoryType3M
+
+  from tb_join_all
+
+  group by idSeller
+  
+),
+
+tb_seller_category as (
+
+  select idSeller,
+         descCategoryName,
+         count(*) as qtCategory
+
+  from tb_join_all
+  group by idSeller, descCategoryName
+
+),
+
+tb_best_category as (
+
+  select *,
+        row_number() over (partition by idSeller order by qtCategory desc) as rankCategory
+  from tb_seller_category
+  qualify rankCategory = 1
 )
 
-select idSeller,
-       avg(vlWeightGramas) as vlAvgWeight,
-       coalesce(avg(case when datediff('{date}', dtPurchase) < 30 then vlWeightGramas end),0) as vlAvgWeight1M,
-       coalesce(avg(case when datediff('{date}', dtPurchase) < 90 then vlWeightGramas end),0) as vlAvgWeight3M,
-       
-       avg(nrNameLength) as vlAvgNameLength,
-       coalesce(avg(case when datediff('{date}', dtPurchase) < 30 then nrNameLength end),0) as vlAvgNameLength1M,
-       coalesce(avg(case when datediff('{date}', dtPurchase) < 90 then nrNameLength end),0) as vlAvgNameLength3M,
-       
-       avg(nrPhotos) as vlAvgPhotos,
-       coalesce(avg(case when datediff('{date}', dtPurchase) < 30 then nrPhotos end),0) as vlAvgPhotos1M,
-       coalesce(avg(case when datediff('{date}', dtPurchase) < 90 then nrPhotos end),0) as vlAvgPhotos3M,
-       
-       avg(vlLengthCm * vlHeightCm * vlWidthCm) as vlAvgProductVolume,
-       avg(case when datediff('{date}', dtPurchase) < 30 then vlLengthCm * vlHeightCm * vlWidthCm else 0 end) as vlAvgProductVolume1M,
-       avg(case when datediff('{date}', dtPurchase) < 90 then vlLengthCm * vlHeightCm * vlWidthCm else 0 end) as vlAvgProductVolume3M,
+select t1.*,
+       t2.descCategoryName
 
-       count(distinct idProduct) as qtProducts,
-       count(distinct case when datediff('{date}', dtPurchase) < 30 then idProduct end) as qtProducts1M,
-       count(distinct case when datediff('{date}', dtPurchase) < 90 then idProduct end) as qtProducts3M,
+from tb_summary as t1
 
-       count(distinct descCategoryName) as qtCategoryType,
-       count(distinct case when datediff('{date}', dtPurchase) < 30 then descCategoryName end) as qtCategoryType1M,
-       count(distinct case when datediff('{date}', dtPurchase) < 90 then descCategoryName end) as qtCategoryType3M
+left join tb_best_category as t2
+on t1.idSeller = t2.idSeller
 
-from tb_join_all
-
-group by idSeller
 
 -- COMMAND ----------
 
